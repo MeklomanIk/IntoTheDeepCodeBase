7a2500aef0931bb74794df540de1387a7da66184
Mikhail Kolesnikov <github@meklomanik.anonaddy.com>
2025-03-02 22:45:17 +0300

    feature (Gyroscope): V0.0.1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Gyroscope.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class IMUSensor {
+    private Telemetry telemetry;
+    private IMU imu;
+
+    public IMUSensor(LinearOpMode opMode) {
+        telemetry = opMode.telemetry;
+        imu = opMode.hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD)));
+        imu.resetYaw();
+    }
+
+    public void resetYaw() {
+        imu.resetYaw();
+    }
+
+    public double getHeading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+    }
+
+    public void addTelemetry() {
+        telemetry.addData("Heading", getHeading());
+    }
+}
